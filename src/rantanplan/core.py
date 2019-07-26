c9f900b2d04917a450f6a6b794db0b2a3835a2b2
Álvaro Pérez <alvaro.perez.pozo@gmail.com>
2019-07-26 10:55:36 +0200

    Feat/new syllabification (#47)
    * Bump version: 0.2.0 → 0.3.0
    
    * Better syllabification
    
    * removed jupyter nb
    
    * better syllabification + tests
    
    * added tests for verbs in -uir
    
    * added tests more exceptions and tests
    
    * added even more exceptions and tests
    
    * added even even more exceptions and tests
    
    * added most of the syllabification rules from Ríos Mestre's DEFE
    
    * added Antonio Ríos Mestre references
    
    * better dictionary lookup
    
    * New phonetic exceptions dictionary for metaplasm candidates
    
    * Bump version: 0.3.0 → 0.4.0
    
    * Fixed typos, tests and structure
    
    * Fixed indentation and alternative syllabification comment

--- src/rantanplan/core.py
+++ src/rantanplan/core.py
@@ -2,11 +2,17 @@
 # Based on previous work done by Rafael C. Carrasco, José A. Mañas (Communications of the ACM 30(7), 1987)
 # and Javier Sober
 # https://github.com/postdataproject/skas-archived/blob/devel/skas/phonmet/syll/grapheme2syllable.py
-
+#
+# Presyllabification and syllabification rules are taken from Antonio Ríos Mestre's
+# 'El Diccionario Electrónico Fonético del Español'
+# https://www.raco.cat/index.php/Elies/article/view/194843
+# http://elies.rediris.es/elies4/Fon2.htm
+# http://elies.rediris.es/elies4/Fon8.htm
 import re
 
 from spacy.tokens import Doc
 
+from .alternative_syllabification import ALTERNATIVE_SYLLABIFICATION
 from .pipeline import load_pipeline
 
 """
@@ -17,17 +23,28 @@ accents_re = re.compile("[áéíóú]", re.I | re.U)
 paroxytone_re = re.compile("([aeiou]|n|[aeiou]s)$", re.I | re.U)  # checks if a str ends in unaccented vowel/N/S
 
 letter_clusters_re = re.compile(r"""
-    ([iuü]h[iuü])|                                            # 1: vowels
-    ([aáeéíoóú]h[iuü])|                                       # 2: open vowels
-    ([iuü]h[aáeéíoóú])|                                       # 3: closed vowels
-    ([a-záéíóúñ][bcdfghjklmnñpqrstvxyz][hlr][aáeéiíoóuúü])|   # 4: liquid and mute consonants (adds hyphen)
-    ([bcdfghjklmnñpqrstvxyz][hlr][aáeéiíoóuúü])|              # 5: any char followed by liquid and mute consonant
-    ([a-záéíóúñ][bcdfghjklmnñpqrstvxyz][aáeéiíoóuúüï])|        # 6: non-liquid consonant (adds hyphen)
-    ([aáeéíoóú][aáeéíoóú])|                                   # 7: vowel group (adds hyphen)
-    (ü[eií])|                                                 # 8: umlaut 'u' diphthongs
-    ([aeiou][äëïöü])|                                         # 9: Explicit hyatus with umlaut vowels, first part
-    ([äëïöü][a-z])|                                           #10: Explicit hyatus with umlaut vowels, second part
-    ([a-záéíóúñ])                                             #11: any char
+    # 1: weak vowels diphthong with h
+    ([iuü]h[iuü])|
+    # 2: open vowels
+    ([aáeéíoóú]h[iuü])|
+    # 3: closed vowels
+    ([iuü]h[aáeéíoóú])|
+    # 4: liquid and mute consonants (adds hyphen)
+    ([a-záéíóúñ](?:(?:[bcdfghjklmnñpqsvxyz][hlr])|(?:[bcdfghjklmnñpqrstvxyz][hr]))[aáeéiíoóuúü])|
+    # 5: any char followed by liquid and mute consonant, exceptions for 'r+l' and 't+l'
+    ((?:(?:[bcdfghjklmnñpqsvxyz][hlr])|(?:[bcdfghjklmnñpqrstvxyz][hr]))[aáeéiíoóuúü])|
+    # 6: non-liquid consonant (adds hyphen)
+    ([a-záéíóúñ][bcdfghjklmnñpqrstvxyz][aáeéiíoóuúüï])|
+    # 7: vowel group (adds hyphen)
+    ([aáeéíoóú][aáeéíoóú])|
+    # 8: umlaut 'u' diphthongs
+    (ü[eií])|
+    # 9: Explicit hiatus with umlaut vowels, first part
+    ([aeiou][äëïöü])|
+    #10: Explicit hiatus with umlaut vowels, second part
+    ([äëïöü][a-z])|
+    #12: any char
+    ([a-záéíóúñ])
 """, re.I | re.U | re.VERBOSE)  # re.VERBOSE is needed to be able to catch the regex group
 
 """
@@ -38,9 +55,32 @@ WEAK_VOWELS = set("iuüíúIÍUÜÚ")
 LIAISON_FIRST_PART = set("aeiouáéíóúAEIOUÁÉÍÓÚy")
 LIAISON_SECOND_PART = set("aeiouáéíóúAEIOUÁÉÍÓÚhy")
 STRESSED_UNACCENTED_MONOSYLLABLES = {"yo", "vio", "dio", "fe", "sol", "ti", "un"}
-UNSTRESSED_UNACCENTED_MONOSYLLABLES = {"me", "nos", "te", "os", "lo", "la", "los", "las", "le",
-                                       "les", "se", "tan", "el", "mas", "te", "si", "tu", "de",
-                                       "mi", "si", "tu", "que", "de", "su"}
+UNSTRESSED_UNACCENTED_MONOSYLLABLES = {'de', 'el', 'la', 'las', 'le', 'les', 'lo', 'los',
+                                       'mas', 'me', 'mi', 'nos', 'os', 'que', 'se', 'si',
+                                       'su', 'tan', 'te', 'tu'}
+
+
+"""
+Regular expressions and rules for syllabification exceptions
+
+"""
+
+# words starting with ABROG-
+ABROG_RE = re.compile("^(ab)(rog[au].*)", re.I | re.U)
+
+# words starting with OBREP-
+OBREP_RE = re.compile("^(ob)(rep.*)", re.I | re.U)
+
+# words starting with prefixes SIN-/DES- followed by consonant "destituir", "sinhueso"
+PREFIXES_WITH_CONSONANT_RE = (re.compile("^(des|sin)([bcdfgjklmhnñpqrstvxyz].*)", re.I | re.U))
+
+# words with prefix EN- followed by consonant: "entrampar", "abencerraje"
+PREFIX_EN_WITH_CONSONANT_RE = (re.compile("(.*?en)([bcdfgjklmhnñpqrstvxyz].*)", re.I | re.U))
+
+# group rh + u[aeiou] dipthongs : "marhuenda"
+PREFIX_RNH_DIPTHONG_RE = (re.compile("(.*?r)(hu[aeioáéíó].*)", re.I | re.U))
+
+
 """
 Metrical Analysis functions
 """
@@ -63,23 +103,56 @@ Syllabifier functions
 """
 
 
-def hyphenate(word):
+def apply_exception_rules(word):
+    # 'Abrogar' and derivatives
+    if ABROG_RE.match(word):
+        match = ABROG_RE.search(word)
+        if match is not None:
+            word = "-".join(match.groups())
+    # 'Obrepticio' and derivatives
+    if OBREP_RE.match(word):
+        match = OBREP_RE.search(word)
+        if match is not None:
+            word = "-".join(match.groups())
+    # Prefix 'en' followed by consonant
+    if PREFIX_EN_WITH_CONSONANT_RE.match(word):
+        match = PREFIX_EN_WITH_CONSONANT_RE.search(word)
+        if match is not None:
+            word = "-".join(match.groups())
+    # Prefix 'des'/'sin' followed by consonant
+    if PREFIXES_WITH_CONSONANT_RE.match(word):
+        match = PREFIXES_WITH_CONSONANT_RE.search(word)
+        if match is not None:
+            word = "-".join(match.groups())
+    # Group rh followed by u + dipthong
+    if PREFIX_RNH_DIPTHONG_RE.match(word):
+        match = PREFIX_RNH_DIPTHONG_RE.search(word)
+        if match is not None:
+            word = "-".join(match.groups())
+    return word
+
+
+def syllabify(word):
     """
-    Hyphenates a word.
-    :param word: The word to be hyphenated.
-    :return: Hyphenation.
+    Syllabifies a word.
+    :param word: The word to be syllabified.
+    :return: list of syllables and exceptions where appropriate.
     :rtype: list
     """
     output = ""
+    original_word = word
+    word = apply_exception_rules(word)
     while len(word) > 0:
         output += word[0]
         # Returns first matching pattern.
         m = letter_clusters_re.search(word)
         if m is not None:
-            # Adds hyphen to syllables if regex pattern is 4, 6, or 7
-            output += "-" if m.lastindex in set([4, 6, 7, 9, 10]) else ""
+            # Adds hyphen to syllables if regex pattern is not 5, 8, 11
+            output += "-" if m.lastindex not in set([5, 8, 11]) else ""
         word = word[1:]
-    return output.split("-")
+    # Remove empty elements created during syllabification
+    output = list(filter(bool, output.split("-")))
+    return output, ALTERNATIVE_SYLLABIFICATION.get(original_word, (None, ()))[1]
 
 
 def get_orthographic_accent(syllable_list):
@@ -134,7 +207,7 @@ def get_word_stress(word, pos, tag):
     :return: Dict with [original syllab word, stressed syllab. word, negative index position of stressed syllable or 0 if not stressed]
     :rtype: dict
     """
-    syllable_list = hyphenate(word)
+    syllable_list, _ = syllabify(word)
     if len(syllable_list) == 1:
         if ((syllable_list[0].lower() not in UNSTRESSED_UNACCENTED_MONOSYLLABLES)
                 and ((syllable_list[0].lower() in STRESSED_UNACCENTED_MONOSYLLABLES)
